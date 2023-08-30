7119cab2ab2e97b3aefaf20faca5201f18d31537
ViTuratti <125040095+ViTuratti@users.noreply.github.com>
2023-08-30 19:17:52 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,26 @@
+import CardDrink from "@/components/CardDrink";
 import Titulo from "@/components/Titulo";
 
 export default function Home() {
+
+  const filmes= [
+    {
+      titulo: "Sobrenatural: A Porta Vermelha",
+      nota: 9.5,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/6lp4uDxLqLEw1CzW1SUOYJ3zdKD.jpg"
+    },
+    {
+      titulo: "Invocação do Mal 3: A Ordem do Demônio",
+      nota: 9.8,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tLmaOXT9rMb17ISQDURKQIu3Okk.jpg"
+    },
+    {
+      titulo: "O Ritual",
+      nota: 7.5,
+      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yFRTz69BvtqZa7VjkyNGehjVb6p.jpg"
+    }
+  ] 
+
   return ( //JSX
   <>
     <nav className="bg-rose-950 p-2 flex gap-3 items-end">
@@ -15,6 +35,10 @@ export default function Home() {
       </ul>
     </nav>
     <Titulo>Cocktails</Titulo>
+    <section className="flex flex-wrap gap-2">
+      {drinks.map( drink => <CardDrink drink={drink} />)}
+    </section>
+
     <Titulo>Tropicais</Titulo>
     </>
   )
